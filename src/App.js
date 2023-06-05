58035e2b0fb7b27242ee0d2b66848ea9421c7ef2
V Suhas <suhas3145@gmail.com>
2023-06-05 17:29:58 +0530

    Basic contents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import styled from "styled-components"
+
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Works from "./components/Works";
+
+const Container = styled.div`
+   height:100vh;
+   background-color: rebeccapurple;
+
+`
+
 
 function App() {
   return (
-    <div>
-      Hello_world
-    </div>
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+    </Container>
   );
 }
 
