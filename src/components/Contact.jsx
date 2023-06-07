b9f25ccd9afe406599abe975ecee6f163c9f9da4
V Suhas <suhas3145@gmail.com>
2023-06-08 02:17:51 +0530

    email message set up done using emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
-import Map from './Map';
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import styled from "styled-components";
+import Map from "./Map";
 
-
-const Section =styled.div`
-   height:100vh;
-   scroll-snap-align:center;
-
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
 `;
 
 const Container = styled.div`
@@ -15,39 +14,34 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   gap: 50px;
-
-
 `;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
-
-
+  @media only screen and (max-width: 768px) {
+    justify-content: center;
+  }
 `;
+
 const Title = styled.h1`
-     font-weight: 200;
-
-
-
+  font-weight: 200;
 `;
+
 const Form = styled.form`
   width: 500px;
   display: flex;
   flex-direction: column;
   gap: 25px;
-   
-   
-   
 `;
+
 const Input = styled.input`
   padding: 20px;
   background-color: #e8e6e6;
   border: none;
   border-radius: 5px;
-
-
 `;
 
 const TextArea = styled.textarea`
@@ -55,12 +49,9 @@ const TextArea = styled.textarea`
   border: none;
   border-radius: 5px;
   background-color: #e8e6e6;
-
-
-
 `;
-const Button = styled.button`
 
+const Button = styled.button`
   background-color: #da4ea2;
   color: white;
   border: none;
@@ -68,44 +59,61 @@ const Button = styled.button`
   cursor: pointer;
   border-radius: 5px;
   padding: 20px;
-
-
 `;
-
-
-
-
-
-
 
 const Right = styled.div`
-   flex:1;
-
+  flex: 1;
 `;
 
-
-
 const Contact = () => {
+  const ref = useRef();
+  const [success, setSuccess] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_yyp168t",
+        "template_rii97jk",
+        ref.current,
+        "6t_kDo7uQEGYqjMpK"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSuccess(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setSuccess(false);
+        }
+      );
+  };
   return (
     <Section>
       <Container>
-      <Left>
-        <Form>
-          <Title>Contact Us</Title>
-          <Input placeholder="Name" name = "name"/>
-          <Input placeholder="Email" name="email"/>
-          <TextArea placeholder="Write your message" rows={10}/>
-          <Button type="submit">Send</Button>
-        </Form>
-      </Left>
-      <Right>
-
-         <Map/>
-
-      </Right>
+        <Left>
+          <Form ref={ref} onSubmit={handleSubmit}>
+            <Title>Contact Us</Title>
+            <Input placeholder="Name" name="name" />
+            <Input placeholder="Email" name="email" />
+            <TextArea
+              placeholder="Write your message"
+              name="message"
+              rows={10}
+            />
+            <Button type="submit">Send</Button>
+            {success &&
+              "Your message has been sent. We'll get back to you soon :)"}
+          </Form>
+        </Left>
+        <Right>
+          <Map />
+        </Right>
       </Container>
     </Section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
