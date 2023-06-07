22e29ef946e4abfacd1b6d6aeadd5a2a0d94e6a2
V Suhas <suhas3145@gmail.com>
2023-06-08 00:58:20 +0530

    Contact page design done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,14 +6,99 @@ const Section =styled.div`
    height:100vh;
    scroll-snap-align:center;
 
-`
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+
+
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+
+`;
+const Title = styled.h1`
+     font-weight: 200;
+
+
+
+`;
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+   
+   
+   
+`;
+const Input = styled.input`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+
+
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #e8e6e6;
+
+
+
+`;
+const Button = styled.button`
+
+  background-color: #da4ea2;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+
+
+`;
+
+
+
+
+
+
+
+const Right = styled.div`
+   flex:1;
+
+`;
 
 
 
 const Contact = () => {
   return (
     <Section>
-      Contact
+      <Container>
+      <Left>
+        <Form>
+          <Title>Contact Us</Title>
+          <Input placeholder="Name" name = "name"/>
+          <Input placeholder="Email" name="email"/>
+          <TextArea placeholder="Write your message" rows={10}/>
+          <Button type="submit">Send</Button>
+        </Form>
+      </Left>
+      <Right></Right>
+      </Container>
     </Section>
   )
 }
